46b68003920f2b64bf8e7dd5599665dbe1c9196e
jinxiuru <jinxiuru@MacBook-Pro-83.local>
2023-03-06 21:11:55 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -4,17 +4,23 @@ username=$1
 password=$2
 horde_url=$3
 api_key=$4
-os=$5
-if [-z $os ];then
-  os="linux"
-fi
+update_requirements=$5
 
 if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] || [ -z $4 ];then
     echo "please input all params"
-    echo "\$1: username"
-    echo "\$2: password"
-    echo "\$3: horde_url"
-    echo "\$4: api_key"
+    echo "\$1: username(required)"
+    echo "\$2: password(required)"
+    echo "\$3: horde_url(required)"
+    echo "\$4: api_key(required)"
+    echo "\$5: if update requirements(Y/N default N)"
+    exit 0
+fi
+
+# print gpu info
+echo "GPU info: >>>"
+nvidia-smi
+if [ $? -ne 0 ];then
+    echo "get driver info failed, please check your driver"
     exit 0
 fi
 
@@ -24,6 +30,7 @@ host_name=`hostname`
 
 echo "set bridge data"
 if [ ! -f bridgeData.py ];then
+    echo "create file bridgeData.py"
 cat << EOF > bridgeData.py
 horde_url = "${horde_url}"
 worker_name = "${host_name}"
@@ -50,7 +57,7 @@ models_to_skip = ['stable_diffusion_inpainting']
 forms = ['caption', 'nsfw']
 EOF
 else
-    echo $horde_url
+    echo "update file bridgeData.py"
     sed -i  "s#^horde_url.*#horde_url = \"${horde_url}\"#g" bridgeData.py
     sed -i  "s#^api_key.*#api_key = \"${api_key}\"#g" bridgeData.py
     sed -i "s#^worker_name.*#worker_name = \"${worker_name}\"#g" bridgeData.py
@@ -59,24 +66,30 @@ fi
 # creds.py
 echo "set creds..."
 if [ ! -f creds.py ];then
-echo ">>>"
+echo "create file creds.py"
 cat << EOF > creds.py
 hf_username = "${username}"
 hf_password = "${password}"
 EOF
 else
-    echo "----"
-    echo $username
+    echo "update file creds.py"
     sed -i "s#^hf_username.*#hf_username = \"${username}\"#g" creds.py
     sed -i "s#^hf_password.*#hf_password = \"${password}\"#g" creds.py
 fi
 
-if [ $os == 'linux' ];then
+if [ ! -f .done ] || [ $update_requirements == "Y" ];then
     echo "start update runtime..."
-    sh update_runtime.sh
-    echo "start worker"
-    sh horde_bridge.sh
-else:
-    echo "please change windows cmd window and execute start.cmd"
+    sh update-runtime.sh
+    if [ $? -eq 0 ];then
+        rm -f .done
+        touch .done
+    else
+      rm -f .done
+      echo "update requirements failed, you can check or retry"
+      exit 0
+    fi
 fi
+echo "start worker"
+sh horde-bridge.sh
+
 
